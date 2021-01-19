7b1b1eb2611fc8b13dd20e6eb9c2187291f4a7e5
Greentaily <greentail@protonmail.ch>
2021-01-19 09:45:48 +0300

    Add CodeParser stub

---
+++ app/src/main/java/com/example/forms/CodeParser.java
@@ -0,0 +1,11 @@
+package com.example.forms;
+
+public class CodeParser {
+    public static String createQuery() {
+        return null;
+    }
+
+    public static String[] parseQuery() {
+        return null;
+    }
+}
