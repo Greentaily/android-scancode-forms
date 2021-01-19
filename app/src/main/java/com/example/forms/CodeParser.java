bbf2ede2cc11f871cc6f6de38525b45da446798c
Greentaily <greentail@protonmail.ch>
2021-01-19 09:53:40 +0300

    Implement CodeParser.createQuery

--- app/src/main/java/com/example/forms/CodeParser.java
+++ app/src/main/java/com/example/forms/CodeParser.java
@@ -1,8 +1,18 @@
 package com.example.forms;
 
 public class CodeParser {
-    public static String createQuery() {
-        return null;
+    public static String createQuery(String serial, String form, String data, int dataIdx, int fields) {
+        StringBuilder query = new StringBuilder("Q1")
+                .append(serial)
+                .append("\u0001")
+                .append(form)
+                .append("\u0002");
+        for (int i = 0; i < fields - 1; i++) {
+            String field = (i == dataIdx ? data + '\t' : String.valueOf('\t'));
+            query.append(field);
+        }
+        query.append('\r');
+        return query.toString();
     }
 
     public static String[] parseQuery() {
