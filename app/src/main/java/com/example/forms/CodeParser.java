ab8daf41b34b62f633fc0d19ff62f16d2df225ee
Greentaily <greentail@protonmail.ch>
2021-01-19 09:56:53 +0300

    Fix query

--- app/src/main/java/com/example/forms/CodeParser.java
+++ app/src/main/java/com/example/forms/CodeParser.java
@@ -3,9 +3,9 @@ package com.example.forms;
 public class CodeParser {
     public static String createQuery(String serial, String form, String data, int dataIdx, int fields) {
         StringBuilder query = new StringBuilder("Q1")
-                .append(serial)
-                .append("\u0001")
                 .append(form)
+                .append("\u0001")
+                .append(serial)
                 .append("\u0002");
         for (int i = 0; i < fields - 1; i++) {
             String field = (i == dataIdx ? data + '\t' : String.valueOf('\t'));
